fd7eea3dde62dd9bf48c9d24d1d3894628a4d284
Harrison <imthatguyh9@gmail.com>
2025-02-24 13:17:57 +1300

    Reordered Algorthims

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -9,6 +9,7 @@ from .cocktail_sort import cocktail_sort
 from .comb_sort import comb_sort
 from .counting_sort import counting_sort
 from .cycle_sort import cycle_sort
+from .exchange_sort import exchange_sort
 from .external_merge_sort import external_merge_sort
 from .flash_sort import flash_sort
 from .franceschinis_method import franceschinis_method
@@ -19,17 +20,17 @@ from .icant_believe_it_can_sort import icant_believe_it_can_sort
 from .insertion_sort import insertion_sort
 from .intro_sort import intro_sort
 from .library_sort import library_sort
-from .merge_sort import merge_sort
 from .merge_insertion_sort import merge_insertion_sort
+from .merge_sort import merge_sort
 from .msd_radix_sort import msd_radix_sort
 from .msd_radix_sort_inplace import msd_radix_sort_inplace
 from .odd_even_sort import odd_even_sort
+from .p_merge_sort import p_merge_sort
 from .pancake_sort import pancake_sort
 from .patience_sort import patience_sort
 from .pigeonhole_sort import pigeonhole_sort
 from .polyphase_merge_sort import polyphase_merge_sort
 from .postman_sort import postman_sort
-from .p_merge_sort import p_merge_sort
 from .quick_sort import quick_sort
 from .radix_sort import radix_sort
 from .replacement_selection_sort import replacement_selection_sort
@@ -41,13 +42,12 @@ from .slowsort import slowsort
 from .smooth_sort import smooth_sort
 from .sorting_network import sorting_network
 from .spaghetti_sort import spaghetti_sort
+from .spreadsort import spreadsort
 from .stooge_sort import stooge_sort
 from .strand_sort import strand_sort
 from .tim_sort import tim_sort
 from .tournament_sort import tournament_sort
 from .tree_sort import tree_sort
-from .exchange_sort import exchange_sort
-from .spreadsort import spreadsort
 
 __all__ = [
     "bead_sort",
@@ -72,17 +72,17 @@ __all__ = [
     "insertion_sort",
     "intro_sort",
     "library_sort",
-    "merge_sort",
     "merge_insertion_sort",
+    "merge_sort",
     "msd_radix_sort",
     "msd_radix_sort_inplace",
     "odd_even_sort",
+    "p_merge_sort",
     "pancake_sort",
     "patience_sort",
     "pigeonhole_sort",
     "polyphase_merge_sort",
     "postman_sort",
-    "p_merge_sort",
     "quick_sort",
     "radix_sort",
     "replacement_selection_sort",
@@ -94,10 +94,10 @@ __all__ = [
     "smooth_sort",
     "sorting_network",
     "spaghetti_sort",
+    "spreadsort",
     "stooge_sort",
     "strand_sort",
     "tim_sort",
     "tournament_sort",
     "tree_sort",
-    "spreadsort",
 ]
